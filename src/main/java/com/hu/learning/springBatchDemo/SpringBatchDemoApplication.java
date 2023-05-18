4254f5671e66655c086397d9526fc64b29382a11
imshashikumarGH <imshashikumar01@gmail.com>
2023-05-18 15:39:05 +0530

    Basic Spring Project

---
+++ src/main/java/com/hu/learning/springBatchDemo/SpringBatchDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hu.learning.springBatchDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hu/learning/springBatchDemo/SpringBatchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hu.learning.springBatchDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
