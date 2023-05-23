68b8e6fdc78d0a7dc8a3e43eaca4761b07d4c0f7
imshashikumarGH <imshashikumar01@gmail.com>
2023-05-23 18:10:14 +0530

    Feature Spring Batch : Partition Class

---
+++ src/main/java/com/hu/learning/springBatchDemo/partition/RangePartitioner.java
@@ -0,0 +1,41 @@
+package com.hu.learning.springBatchDemo.partition;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.partition.support.Partitioner;
+import org.springframework.batch.item.ExecutionContext;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+public class RangePartitioner implements Partitioner {
+    @Override
+    public Map<String, ExecutionContext> partition(int gridSize) {
+        int min = 1;
+        int max = 1000;
+        int targetSize = (max - min) / gridSize + 1;//500
+        log.debug("targetSize : " + targetSize);
+        Map<String, ExecutionContext> result = new HashMap<>();
+
+        int number = 0;
+        int start = min;
+        int end = start + targetSize - 1;
+        //1 to 500
+        // 501 to 1000
+        while (start <= max) {
+            ExecutionContext value = new ExecutionContext();
+            result.put("partition" + number, value);
+
+            if (end >= max) {
+                end = max;
+            }
+            value.putInt("minValue", start);
+            value.putInt("maxValue", end);
+            start += targetSize;
+            end += targetSize;
+            number++;
+        }
+        log.info("partition result:" + result.toString());
+        return result;
+    }
+}
