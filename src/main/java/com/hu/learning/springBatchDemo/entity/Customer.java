4c0caee230db6cc5e3767508f301867879642922
imshashikumarGH <imshashikumar01@gmail.com>
2023-05-23 12:20:47 +0530

    Feature Spring Batch : code refactor setters getters constructor etc

--- src/main/java/com/hu/learning/springBatchDemo/entity/Customer.java
+++ src/main/java/com/hu/learning/springBatchDemo/entity/Customer.java
@@ -5,10 +5,18 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.*;
 
 @Entity
 @Table
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
+
+
     @Id
     @Column(name = "CUST_ID")
     private int id;
